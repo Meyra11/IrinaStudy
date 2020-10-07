6b67dce816c4ff07e8a590a59b84e0d5968e179c
greyewi1 <greyewi@gmail.com>
2020-10-07 20:23:29 +0300

    add renderTable

--- TableProject/scripts/script1.js
+++ TableProject/scripts/script1.js
@@ -1,38 +1,84 @@
 "use strict"
 
+let mainData = []
+
+const renderTable = (tableData) => {
+    const tbody = document.getElementById("employees-table").getElementsByTagName('TBODY')[0];
+    tbody.innerHTML = ""
+
+    tableData.map((item) => {
+
+        let row = document.createElement("TR");
+        row.classList.add("employees-row");
+        tbody.appendChild(row);
+
+        // TODO сделать цикл DRY
+        const td1 = document.createElement("TD");
+        const td2 = document.createElement("TD");
+        const td3 = document.createElement("TD");
+        const td4 = document.createElement("TD");
+        const td5 = document.createElement("TD");
+
+        // TODO сделать цикл DRY
+        row.appendChild(td1);
+        row.appendChild(td2);
+        row.appendChild(td3);
+        row.appendChild(td4);
+        row.appendChild(td5);
+
+        // Наполняем ячейки
+        // TODO сделать цикл DRY
+        td1.innerHTML = item.id;
+        td2.innerHTML = item.name;
+        td3.innerHTML = item.position;
+        td4.innerHTML = item.gender;
+        td5.innerHTML = item.age;
+    })
+}
+
+
 function addRow()
 {
     // Считываем значения с формы
     const name = document.getElementById("name").value;
     const position = document.getElementById("position").value;
+    const gender = document.getElementById("gender").value;
     const age = document.getElementById("age").value;
 
-    // Находим таблицу
-    const tbody = document.getElementById("employees-table").getElementsByTagName('TBODY')[0];
+    const newObj = [{"id": mainData.length + 1, "name": name, "position": position, "gender": gender, "age": age}]
+    mainData = mainData.concat(newObj)
 
-    // Создаем строку таблицы и добавляем ее
-    let row = document.createElement("TR");
-    row.classList.add("employees-row");
-    tbody.appendChild(row);
+    renderTable(mainData)
 
-    // Создаем ячейки в вышесозданной строке и добавляем
-    const td1 = document.createElement("TD");
-    const td2 = document.createElement("TD");
-    const td3 = document.createElement("TD");
-    const td4 = document.createElement("TD");
-    const td5 = document.createElement("TD");
+    // TODO добавить очистку полей ввода
+
+}
 
 
-    row.appendChild(td1);
-    row.appendChild(td2);
-    row.appendChild(td3);
-    row.appendChild(td4);
-    row.appendChild(td5);
+const handleSortIncrease = (field) => {
+    renderTable(mainData.sort((a, b) => {
+        if(a[field] < b[field]) {
+           return -1
+        } else if(a[field] > b[field]){
+            return 1
+        } else {
+            return 0
+        }
+    }))
+    // TODO изменить изображения
+}
 
-    // Наполняем ячейки
-    td2.innerHTML = name;
-    td3.innerHTML = position;
-    td5.innerHTML = age;
+const handleSortDecrease = (field) => {
+    renderTable(mainData.sort((a, b) => {
+        if(a[field] < b[field]) {
+           return 1
+        } else if(a[field] > b[field]){
+            return -1
+        } else {
+            return 0
+        }
+    }))
+    // TODO изменить изображения
 }
 
 function loadPage() {
@@ -40,4 +86,7 @@ function loadPage() {
   btn.onclick = addRow;
   btn.style.backgroundColor = "#3333"
 
+    fetch('https://gist.githubusercontent.com/Greyewi/c339c8b4f785a27471b42d46a5d076ab/raw/f236135f1d7cf57dd71aca6d5d92301ae46f2142/Irina_table.json')
+      .then(response => response.json())
+      .then(result => renderTable(mainData = result))
 }
